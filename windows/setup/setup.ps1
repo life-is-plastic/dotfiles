135fa7d925bd6993b4d9df9fcb0918a17405a5be
Life is Plastic <its@fan.tastic>
2025-01-24 22:14:11 -0500

    update windows ssh

--- windows/setup/setup.ps1
+++ windows/setup/setup.ps1
@@ -1,3 +1,7 @@
+if (-not (Test-Path -Path $HOME\.ssh)) {
+    New-Item -Type Directory $HOME\.ssh
+}
+
 if (-not (Test-Path -Path $HOME\.ssh\id_ed25519)) {
     ssh-keygen -t ed25519 -f $HOME\.ssh\id_ed25519 -C '""'
 }
