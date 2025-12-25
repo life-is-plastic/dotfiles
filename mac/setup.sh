8f003f493835689e558d5d0c70c9676cd6b9a0e3
Life is Plastic <its@fan.tastic>
2025-12-25 13:37:38 -0500

    doc animation timing

---
+++ mac/setup.sh
@@ -0,0 +1,6 @@
+# Dock controls
+defaults write com.apple.dock autohide-time-modifier -float 0.167
+defaults write com.apple.dock autohide-delay -float 0
+# defaults delete com.apple.dock autohide-time-modifier
+# defaults delete com.apple.dock autohide-delay
+killall Dock
