769207b682879ed635a18b48703e267131a83252
Life is Plastic <its@fan.tastic>
2025-12-27 22:59:24 -0500

    mac finder show hidden files

--- mac/setup.sh
+++ mac/setup.sh
@@ -1,6 +1,13 @@
-# Dock controls
+# Delete an override with:
+#   defaults delete <section> <setting>
+# For example:
+#   defaults delete com.apple.dock autohide-time-modifier
+
+# Dock
 defaults write com.apple.dock autohide-time-modifier -float 0.167
 defaults write com.apple.dock autohide-delay -float 0
-# defaults delete com.apple.dock autohide-time-modifier
-# defaults delete com.apple.dock autohide-delay
 killall Dock
+
+# Finder
+defaults write com.apple.finder AppleShowAllFiles -boolean true
+killall Finder
